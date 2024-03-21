b329bf862644fd43c746d095e1d871d2e51e097a
Sankalp Sharma <sankalpshrm1111@gmail.com>
2024-03-22 00:10:56 +0530

    img

--- src/containers/MarketStories.jsx
+++ src/containers/MarketStories.jsx
@@ -1,6 +1,6 @@
 import marketStories from '@/content/market-stories';
 
-
+import Image from 'next/image';
 const MarketStories = ({ className }) => {
   return (
     <section className={`relative flex-grow w-3/12 p-5 ${className}`}>
@@ -9,7 +9,7 @@ const MarketStories = ({ className }) => {
       <div className='flex flex-col h-[100vh] xs:h-[100vh]  md:h-[(100vh)] py-8 gap-5 overflow-scroll no-scrollbar '>
         {marketStories.map(({ img, text, id, url }) => (
           <div className=" rounded  shadow-lg">
-          <Image className="w-full h-{10%} overflow-hidden " src={img} alt="Sunset in the mountains"/>
+          <Image className="w-full h-{10%} overflow-hidden " src={img} alt="Sunset in the mountains" width={300} height={300}/>
           <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
             <p className="text-grey-700 text-base">
