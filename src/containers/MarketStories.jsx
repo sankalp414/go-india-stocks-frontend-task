05b7510b9ea50b3efebed32f803fd6c203de3c5c
Sankalp Sharma <sankalpshrm1111@gmail.com>
2024-03-22 10:58:58 +0530

    link

--- src/containers/MarketStories.jsx
+++ src/containers/MarketStories.jsx
@@ -1,15 +1,20 @@
 import marketStories from '@/content/market-stories';
 
 import Image from 'next/image';
+import Link from 'next/link';
 //marketstories container
 const MarketStories = ({ className }) => {
   return (
-    <section className={`relative flex-grow w-3/12 p-5 ${className}`} key={marketStories.id}>
+    <section className={`relative flex-grow w-3/12 p-5 ${className}`}>
       <h1 className='hidden heading-primary md:block'>market stories</h1>
 
       <div className='flex flex-col h-[100vh] xs:h-[100vh]  md:h-[(100vh)] py-8 gap-5 overflow-scroll no-scrollbar '>
-        {marketStories.map(({ img, text }) => (
-          <div className=" rounded  shadow-lg" >
+        {marketStories.map(({ img, text,url,id }) => (
+          <Link
+           key={id}
+          href={url}
+          target='_blank'
+          className=" rounded  shadow-lg" >
           <Image className="w-full h-{10%} overflow-hidden " src={img} alt="Sunset in the mountains" width={300} height={300}/>
           <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
@@ -17,7 +22,7 @@ const MarketStories = ({ className }) => {
              {text}
             </p>
           </div>
-          </div>
+          </Link>
         ))}
       </div>
     </section>
